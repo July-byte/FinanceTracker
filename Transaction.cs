001fee331be92f0d667fe4fcf4da105a36b02583
Yulia Zavodskova <uliazavodskova@gmail.com>
2026-04-18 19:19:51 +0300

    Create Transaction.cs

---
+++ Transaction.cs
@@ -0,0 +1,26 @@
+using System;
+namespace FinanceTracker
+{
+  public class Transaction
+  {
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public string Category { get; set; }
+    public TransactionType { get; set; }
+    public DateTime Date { get; set; }
+
+    public Transaction (int id, decimal amount, string category, TransactionType type)
+    {
+      Id = id;
+      Amount = amount;
+      Category = category;
+      Type = type;
+      Date = DateTime.Now;
+    }
+    public override string ToString()
+    {
+      string sign = Type == TransactionType.Expense ? "-" : "+";
+      return $"{Id}. {Date:g} | {Category} | {Sign} | {Amount}";
+    }
+  }
+}
