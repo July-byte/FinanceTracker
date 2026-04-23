87228fc0cabc2ef805fa6b50646c83522e14e345
Yulia Zavodskova <uliazavodskova@gmail.com>
2026-04-23 20:42:02 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using System;
+namespace FinanceTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      FinanceManager manager = new FinanceManager();
+      while (true)
+      {
+        Console.WriteLine("\n1 - Добавить доход");
+        Console.WriteLine("2 - Добавить расход");
+        Console.WriteLine("3 - Показать все операции");
+        Console.WriteLine("4 - Показать баланс");
+        Console.WriteLine("5 - Фильтр по категории");
+        Console.WriteLine("6 - Выход");
+        Console.WriteLine("Выбор: ");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+          case "1":
+            AddTransaction(manager, TransactionType.Income);
+            break;
+          case "2":
+            AddTransaction(manager, TransactionType.Expense);
+            break;
+          case "3":
+            manager.ShowAllTransactions();
+            break;
+          case "4":
+            manager.ShowBalance();
+            break;
+          case "5":
+            Console.WriteLine("Введите категорию");
+            string category = Console.ReadLine();
+            manager.ShowByCategory(category);
+            break;
+          case "0":
+            return;
+        }
+      }
+    }
+    static void AddTransaction(FinanceManager manager, TransactionType type)
+    {
+      Console.Write("Введите сумму: ");
+      if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
+      {
+        Console.WriteLine("Некорректная сумма");
+        return;
+      }
+      Console.Write("Введите категорию: ");
+      string category = Console.ReadLine();
+      manager.AddTransaction(amount, category, type);
+    }
+  }
+}
+
